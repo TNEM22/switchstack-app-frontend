af9df169553d3be07fdb9ed53ce978c959f50bc8
tnem <tanmay.e.mahale@gmail.com>
2025-06-10 10:34:32 +0530

    url-modified

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
-export const SERVER_URL = 'http://localhost:3000';
+// export const SERVER_URL = 'http://localhost:3000';
 // export const SERVER_URL = 'https://switchstack-app-backend.onrender.com';
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
